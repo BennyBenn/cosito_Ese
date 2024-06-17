1a52891a8a195aee84b034bbc094e519544153c8
BennyBenn <rhadonizer@gmail.com>
2024-06-17 17:46:24 -0600

    a

---
+++ Data_Digester/Data_Digester/Class1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Web.UI.WebControls;
+using System.Collections.Generic;
+
+
+namespace Data_Digester{
+    class Class1{
+        /*
+        public void volcar_excel(){
+
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+            var workbook = excelApp.Workbooks.Open(@"C:\Users\ENTIDADES1\Documents\Visual Studio 2012\Projects\cosito_Ese\Data_Digester\EXCEL\Catalogos_2024.xlsx");
+            var worksheet = workbook.Worksheets[1];
+
+            var range = worksheet.UsedRange;
+            var data = (range.Value as object[,]).Select(x => x.ToString()).ToList();
+
+            var gridView = new DataGridView();
+            gridView.DataSource = data;
+            gridView.DataBind();
+        }
+         * */
+    }
+}
+
+
+/*
+ 
+ // Load the Excel file
+var excelApp = new Microsoft.Office.Interop.Excel.Application();
+var workbook = excelApp.Workbooks.Open(@"path\to\your\file.xlsx");
+var worksheet = workbook.Worksheets[1];
+
+// Get the data
+var range = worksheet.UsedRange;
+var data = (range.Value as object[,]).Select(x => x.ToString()).ToList();
+
+// Dump it onto a GridView
+var gridView = new GridView();
+gridView.DataSource = data;
+gridView.DataBind();
+ * 
+ * 
+ */
--- Data_Digester/Data_Digester/Form1.Designer.cs
+++ Data_Digester/Data_Digester/Form1.Designer.cs
@@ -28,27 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pruebasDataSet = new Data_Digester.pruebasDataSet();
+            this.rAMOBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.rAMOTableAdapter = new Data_Digester.pruebasDataSetTableAdapters.RAMOTableAdapter();
+            this.aÑODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rAMODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dESCRIPCIÓNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pruebasDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rAMOBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(75, 94);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.aÑODataGridViewTextBoxColumn,
+            this.rAMODataGridViewTextBoxColumn,
+            this.dESCRIPCIÓNDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.rAMOBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 32);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(401, 127);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pruebasDataSet
+            // 
+            this.pruebasDataSet.DataSetName = "pruebasDataSet";
+            this.pruebasDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // rAMOBindingSource
+            // 
+            this.rAMOBindingSource.DataMember = "RAMO";
+            this.rAMOBindingSource.DataSource = this.pruebasDataSet;
+            // 
+            // rAMOTableAdapter
+            // 
+            this.rAMOTableAdapter.ClearBeforeFill = true;
+            // 
+            // aÑODataGridViewTextBoxColumn
+            // 
+            this.aÑODataGridViewTextBoxColumn.DataPropertyName = "AÑO";
+            this.aÑODataGridViewTextBoxColumn.HeaderText = "AÑO";
+            this.aÑODataGridViewTextBoxColumn.Name = "aÑODataGridViewTextBoxColumn";
+            // 
+            // rAMODataGridViewTextBoxColumn
+            // 
+            this.rAMODataGridViewTextBoxColumn.DataPropertyName = "RAMO";
+            this.rAMODataGridViewTextBoxColumn.HeaderText = "RAMO";
+            this.rAMODataGridViewTextBoxColumn.Name = "rAMODataGridViewTextBoxColumn";
+            // 
+            // dESCRIPCIÓNDataGridViewTextBoxColumn
+            // 
+            this.dESCRIPCIÓNDataGridViewTextBoxColumn.DataPropertyName = "DESCRIPCIÓN";
+            this.dESCRIPCIÓNDataGridViewTextBoxColumn.HeaderText = "DESCRIPCIÓN";
+            this.dESCRIPCIÓNDataGridViewTextBoxColumn.Name = "dESCRIPCIÓNDataGridViewTextBoxColumn";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(684, 348);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pruebasDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rAMOBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +119,14 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private pruebasDataSet pruebasDataSet;
+        private System.Windows.Forms.BindingSource rAMOBindingSource;
+        private pruebasDataSetTableAdapters.RAMOTableAdapter rAMOTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aÑODataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rAMODataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dESCRIPCIÓNDataGridViewTextBoxColumn;
+
     }
 }
 
